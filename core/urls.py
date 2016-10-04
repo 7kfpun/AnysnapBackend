e3a664ee1473107a0cf933f14d3f25caea858070
kf <7kfpun@gmail.com>
2016-10-04 21:34:21 +0800

    Use tail url

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^users/(?P<user_id>[\w-]+)/images/$', views.get_images),
-    url(r'^users/(?P<user_id>[\w-]+)/images/(?P<image_id>[\w-]+)$', views.get_image),
+    url(r'^users/(?P<user_id>[\w-]+)/images/(?P<image_id>[\w-]+)/$', views.get_image),
 
     url(r'^public_images/$', views.get_public_images),
 ]
