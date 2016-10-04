2512b8c03b270a440bb183a403f9f6fb195345cc
kf <7kfpun@gmail.com>
2016-10-04 20:52:44 +0800

    Analyse after uploading

--- core/views.py
+++ core/views.py
@@ -65,6 +65,7 @@ def get_images(request, user_id):
         user, _ = CustomUser.objects.get_or_create(id=user_id)
         image.user = user
         image.save()
+        image.analyze(save=True)
         results = [{
             'id': image.pk,
             'user_id': image.user.pk,
